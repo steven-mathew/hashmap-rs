92e9d4198dd86dcd592f003fe3e0dd9e7e863d6f
Steven Mathew <ste.tho.mat@gmail.com>
2021-12-26 19:40:17 -0500

    get works

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,19 @@
 use std::collections::hash_map::DefaultHasher;
 use std::hash::{Hash, Hasher};
+use std::mem;
 
 const INITIAL_NBUCKETS: usize = 1;
 
 pub struct HashMap<K, V> {
     buckets: Vec<Vec<(K, V)>>,
+    items: usize,
 }
 
 impl<K, V> HashMap<K, V> {
     pub fn new() -> Self {
         HashMap {
             buckets: Vec::new(),
+            items: 0,
         }
     }
 }
@@ -19,24 +22,33 @@ impl<K, V> HashMap<K, V>
 where
     K: Hash + Eq,
 {
-    pub fn insert(&mut self, key: K, value: V) -> Option<V> {
+    fn bucket(&self, key: &K) -> usize {
         let mut hasher = DefaultHasher::new();
         key.hash(&mut hasher);
-        let bucket: usize = (hasher.finish() % self.buckets.len() as u64) as usize;
-        let bucket = &mut self.buckets[bucket];
+        (hasher.finish() % self.buckets.len() as u64) as usize
+    }
 
-        for &mut (ref ekey, ref mut evalue) in bucket.iter_mut() {
-            if ekey == &key {
-                use std::mem;
-                return Some(mem::replace(evalue, value));
-            }
+    pub fn insert(&mut self, key: K, value: V) -> Option<V> {
+        if self.buckets.is_empty() || self.items > 3 * self.buckets.len() / 4 {
+            self.resize();
         }
 
+        let bucket = self.bucket(&key);
+        let bucket = &mut self.buckets[bucket];
+
+        // for &mut (ref ekey, ref mut evalue) in bucket.iter_mut() {
+        //     if ekey == &key {
+        //         use std::mem;
+        //         return Some(mem::replace(evalue, value));
+        //     }
+        // }
+
+        self.items += 1;
         // TODO: trying match
         if let Some(&mut (ref _ekey, ref mut evalue)) =
             bucket.iter_mut().find(|&&mut (ref ekey, _)| ekey == &key)
         {
-            use std::mem;
+            // use std::mem;
             return Some(mem::replace(evalue, value));
         } else {
             bucket.push((key, value));
@@ -47,10 +59,43 @@ where
         // None
     }
 
+    pub fn get(&self, key: &K) -> Option<&V> {
+        let bucket = self.bucket(key);
+        self.buckets[bucket]
+            .iter()
+            .find(|&(ref ekey, _)| ekey == key)
+            .map(|&(_, ref v)| v)
+    }
+
     fn resize(&mut self) {
         let target_size = match self.buckets.len() {
             0 => INITIAL_NBUCKETS,
             n => 2 * n,
         };
+
+        let mut new_buckets = Vec::with_capacity(target_size);
+        new_buckets.extend((0..target_size).map(|_| Vec::new()));
+
+        for (key, value) in self.buckets.iter_mut().flat_map(|bucket| bucket.drain(..)) {
+            let mut hasher = DefaultHasher::new();
+            key.hash(&mut hasher);
+            let bucket: usize = (hasher.finish() % new_buckets.len() as u64) as usize;
+            new_buckets[bucket].push((key, value));
+        }
+
+        mem::replace(&mut self.buckets, new_buckets);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+
+    fn insert() {
+        let mut map = HashMap::new();
+        map.insert("foo", 42);
+        assert_eq!(map.get(&"foo"), Some(&42));
     }
 }
